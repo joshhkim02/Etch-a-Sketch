39951d1f0eb474beb8bfa830ade17b56d82d73a8
joshhkim02 <97993478+joshhkim02@users.noreply.github.com>
2024-07-23 14:19:12 -0700

    feat: created 16x16 grid for future drawing

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const containerSelector = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    let div = document.createElement("div");
+    containerSelector.append(div);
+  }
+}
