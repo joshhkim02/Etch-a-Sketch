afd93e3c06454ef6771518703c5be364c2a49b62
joshhkim02 <97993478+joshhkim02@users.noreply.github.com>
2024-07-24 16:17:34 -0700

    feat: added board resizing functionality

--- script.js
+++ script.js
@@ -3,28 +3,51 @@
 // Initialize grid
 const containerSelector = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let addDiv = document.createElement("div");
-    addDiv.className = "gridItem";
-    containerSelector.append(addDiv);
-  }
+for (let i = 0; i < 16 * 16; i++) {
+  let addDiv = document.createElement("div");
+  addDiv.style.height = `calc(100% / 16)`;
+  addDiv.style.flex = `1 0 calc(100% / 16)`;
+  addDiv.className = "gridItem";
+  containerSelector.append(addDiv);
 }
 
 // Hover effect
-const gridItems = document.querySelectorAll(".gridItem");
+let gridItems = document.querySelectorAll(".gridItem");
 
 gridItems.forEach((item) => {
   item.addEventListener("mouseenter", () => {
-    item.setAttribute("style", "background-color:blue");
+    item.style.backgroundColor = "blue";
   });
 });
 
-// Board resizing
+// Resize button functionality
 const btnResizeSelector = document.querySelector(".btn-resize");
 
 btnResizeSelector.addEventListener("click", () => {
+  let inputDimension = Number(prompt("Enter a number up to 100:"));
+
+  // Delete old grid
+  gridItems = document.querySelectorAll(".gridItem");
+
   gridItems.forEach((item) => {
     item.remove();
+  });
+
+  // Create new grid
+  for (let i = 0; i < Number(inputDimension) * Number(inputDimension); i++) {
+    let addDiv = document.createElement("div");
+    addDiv.className = "gridItem";
+    addDiv.style.height = `calc(100% / ${inputDimension})`;
+    addDiv.style.flex = `1 0 calc(100% / ${inputDimension})`;
+    containerSelector.append(addDiv);
+  }
+
+  // Hover effect
+  gridItems = document.querySelectorAll(".gridItem");
+
+  gridItems.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      item.style.backgroundColor = "blue";
+    });
   });
 });
